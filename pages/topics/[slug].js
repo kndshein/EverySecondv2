5de79633188c11c4daf9df6f4a3cf3d51a4f1e3f
Kaung Shein <nicholasling1412@gmail.com>
2021-10-04 00:48:29 -0400

    getStaticProps setup, ready for components

--- pages/topics/[slug].js
+++ pages/topics/[slug].js
@@ -2,9 +2,98 @@ import { GraphQLClient, gql } from "graphql-request";
 
 const client = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_URL);
 
-export default function Topic() {
+export default function Topic({ data: { topic } }) {
+  console.log(topic);
   return <div>Topic</div>;
 }
+
+export const getStaticProps = async ({ params }) => {
+  const slug = params.slug;
+
+  const query = gql`
+    query Topic($slug: String!) {
+      topic(where: { slug: $slug }) {
+        slideOne {
+          seconds
+          tagline
+          taglineCitation
+          icon
+          background {
+            image
+            alt
+            artist
+          }
+        }
+        slideTwo {
+          taglineOne
+          taglineOneCitation
+          taglineTwo
+          taglineTwoCitation
+          icon
+          highlightNum
+          totalNum
+          background {
+            image
+            alt
+            artist
+          }
+        }
+        slideThree {
+          taglineOne
+          taglineOneCitation
+          taglineTwo
+          taglineTwoFocus
+          taglineTwoCitation
+          hexColor {
+            hex
+          }
+          background {
+            image
+            alt
+            artist
+          }
+        }
+        slideFour {
+          educates {
+            name
+            link
+          }
+          awarenesses {
+            name
+            link
+          }
+          volunteers {
+            name
+            link
+          }
+          financials {
+            name
+            link
+          }
+          background {
+            image
+            alt
+            artist
+          }
+        }
+      }
+    }
+  `;
+
+  const data = await client.request(query, { slug });
+
+  console.log(data);
+
+  if (!data.topic) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { data },
+  };
+};
 
 export const getStaticPaths = async () => {
   const query = gql`
