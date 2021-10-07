06f6894f9b04075d6c53a843d72a6e9c01c548ef
Kaung Shein <nicholasling1412@gmail.com>
2021-10-07 00:58:02 -0400

    SlideThree refactored

--- components/SlideThree.js
+++ components/SlideThree.js
@@ -1,50 +1,50 @@
 import { motion } from "framer-motion";
 import useOnScreen from "../utilities/useOnScreen";
+import Background from "./Background";
 
 export default function SlideThree({ content }) {
   const [setRef, visible] = useOnScreen({ threshold: 0.5 }, true);
 
-  const variants = {
-    initial: { opacity: 0 },
-    animate: {
-      opacity: 1,
-      transition: {
-        duration: 1,
-        delay: 0,
+  const animation = {
+    slideContainer: {
+      initial: { opacity: 0 },
+      animate: {
+        opacity: 1,
+        transition: {
+          duration: 1,
+          delay: 0,
+        },
       },
     },
-  };
-
-  const taglineOneVariants = {
-    initial: { opacity: 0 },
-    animate: {
-      opacity: 1,
-      transition: {
-        duration: 2,
-        delay: 1,
+    taglineOne: {
+      initial: { opacity: 0 },
+      animate: {
+        opacity: 1,
+        transition: {
+          duration: 2,
+          delay: 1,
+        },
       },
     },
-  };
-
-  const taglineTwoVariants = {
-    initial: { opacity: 0 },
-    animate: {
-      opacity: 1,
-      transition: {
-        duration: 2,
-        delay: 3,
+    taglineTwo: {
+      initial: { opacity: 0 },
+      animate: {
+        opacity: 1,
+        transition: {
+          duration: 2,
+          delay: 3,
+        },
       },
     },
-  };
-
-  const spanVariants = {
-    initial: { color: "#ffffff" },
-    animate: {
-      color: content.hexColor.hex,
-      transition: {
-        duration: 2,
-        delay: 4.5,
-        ease: "easeInOut",
+    taglineTwoFocus: {
+      initial: { color: "#ffffff" },
+      animate: {
+        color: content.hexColor.hex,
+        transition: {
+          duration: 2,
+          delay: 4.5,
+          ease: "easeInOut",
+        },
       },
     },
   };
@@ -52,39 +52,37 @@ export default function SlideThree({ content }) {
   return (
     <div className="slides">
       <motion.div
-        variants={variants}
+        variants={animation.slideContainer}
         animate={visible ? "animate" : "initial"}
         className="slide-three-container"
         ref={setRef}
       >
         <div className="taglines-container">
           <motion.div
-            variants={taglineOneVariants}
+            variants={animation.taglineOne}
             animate={visible ? "animate" : "initial"}
             className="tagline"
           >
             {content.taglineOne}
           </motion.div>
           <motion.div
-            variants={taglineTwoVariants}
+            variants={animation.taglineTwo}
             animate={visible ? "animate" : "initial"}
             className="tagline-2"
           >
             {content.taglineTwo}{" "}
             <motion.span
-              variants={spanVariants}
+              variants={animation.taglineTwoFocus}
               animate={visible ? "animate" : "initial"}
             >
               {content.taglineTwoFocus}
             </motion.span>
           </motion.div>
         </div>
-        <div className="background-image">
-          <img
-            src={`${content.background.image}`}
-            alt={`${content.background.alt}`}
-          />
-        </div>
+        <Background
+          link={content.background.image}
+          alt={content.background.alt}
+        />
       </motion.div>
     </div>
   );
