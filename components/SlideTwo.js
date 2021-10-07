359bfffc4e118787428b49af070ae9a660cf0e29
Kaung Shein <nicholasling1412@gmail.com>
2021-10-07 00:44:37 -0400

    SlideTwo refactor

--- components/SlideTwo.js
+++ components/SlideTwo.js
@@ -4,106 +4,94 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useOnScreen from "../utilities/useOnScreen";
 
 export default function SlideTwo({ content }) {
-  var num = [],
-    i = 0,
-    len = content.totalNum;
-  while (++i <= len) num.push(i);
   const [setRef, visible] = useOnScreen({ threshold: 0.5 }, true);
   const [resizer, setResizer] = useState({ vw: 0, px: 0 });
 
   useEffect(() => {
-    const calcSize = (num) => {
-      let vw = 0;
-      let px = 0;
-      vw = Math.round(29.863 * Math.pow(num, -0.479) * 10) / 10;
-      console.log("vw", vw);
-      px = Math.round(298.68 * Math.pow(num, -0.479) * 10) / 10;
-      setResizer({ vw: vw, px: px });
-    };
-    calcSize(content.totalNum);
-  }, []);
+    setResizer({
+      vw: Math.round(29.863 * Math.pow(content.totalNum, -0.479) * 10) / 10,
+      px: Math.round(298.68 * Math.pow(content.totalNum, -0.479) * 10) / 10,
+    });
+  }, [content.totalNum]);
 
-  const variants = {
-    initial: { opacity: 0 },
-    animate: {
-      opacity: 1,
-      transition: {
-        duration: 1,
-        delay: 0,
+  const animation = {
+    slideContainer: {
+      initial: { opacity: 0 },
+      animate: {
+        opacity: 1,
+        transition: {
+          duration: 1,
+          delay: 0,
+        },
       },
     },
-  };
-
-  const taglineOneVariants = {
-    initial: { opacity: 0, y: 50 },
-    animate: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 2,
-        delay: 1,
+    taglineOneVariants: {
+      initial: { opacity: 0, y: 50 },
+      animate: {
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 2,
+          delay: 1,
+        },
       },
     },
-  };
-
-  const taglineTwoVariants = {
-    initial: { opacity: 0, y: 50 },
-    animate: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 2,
-        delay: 3,
+    taglineTwoVariants: {
+      initial: { opacity: 0, y: 50 },
+      animate: {
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 2,
+          delay: 3,
+        },
       },
     },
-  };
-
-  const iconsVariantsContainer = {
-    initial: { opacity: 0 },
-    animate: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.002,
-        delayChildren: 5,
-        ease: "easeOut",
+    iconsContainer: {
+      initial: { opacity: 0 },
+      animate: {
+        opacity: 1,
+        transition: {
+          staggerChildren: 0.002,
+          delayChildren: 5,
+          ease: "easeOut",
+        },
       },
     },
-  };
-
-  const iconsVariantsChild = {
-    initial: { opacity: 0 },
-    animate: {
-      opacity: 1,
+    icon: {
+      initial: { opacity: 0 },
+      animate: {
+        opacity: 1,
+      },
     },
-  };
-
-  const iconsVariantsChildFocus = {
-    initial: { opacity: 0, x: -50 },
-    animate: {
-      opacity: 1,
-      x: 0,
-      transition: { duration: 3, delay: 5, ease: "easeOut" },
+    iconFocus: {
+      initial: { opacity: 0, x: -50 },
+      animate: {
+        opacity: 1,
+        x: 0,
+        transition: { duration: 3, delay: 5, ease: "easeOut" },
+      },
     },
   };
 
   return (
     <div className="slides">
       <motion.div
-        variants={variants}
+        variants={animation.slideContainer}
         animate={visible ? "animate" : "initial"}
         className="slide-two-container"
         ref={setRef}
       >
         <div className="taglines-container">
           <motion.div
-            variants={taglineOneVariants}
+            variants={animation.taglineOneVariants}
             animate={visible ? "animate" : "initial"}
             className="tagline"
           >
             {content.taglineOne}
           </motion.div>
           <motion.div
-            variants={taglineTwoVariants}
+            variants={animation.taglineTwoVariants}
             animate={visible ? "animate" : "initial"}
             className="tagline-2"
           >
@@ -111,37 +99,29 @@ export default function SlideTwo({ content }) {
           </motion.div>
         </div>
         <motion.div
-          variants={iconsVariantsContainer}
+          variants={animation.iconsContainer}
           initial="initial"
           animate={visible ? "animate" : "initial"}
           className="icons-container"
         >
-          {num.map((x, index) => {
-            if (index < content.highlightNum) {
-              return (
-                <motion.div
-                  variants={iconsVariantsChildFocus}
-                  className="icon-container"
-                  style={{
-                    width: `calc(max(${resizer.vw}vw,${resizer.px}px))`,
-                  }}
-                  key={index}
-                >
-                  <FontAwesomeIcon
-                    className="icon focus"
-                    icon={content.icon.split(" ")}
-                  />
-                </motion.div>
-              );
-            }
+          {[...new Array(content.totalNum).fill(1)].map((_, index) => {
             return (
               <motion.div
-                variants={iconsVariantsChild}
+                variants={
+                  index < content.highlightNum
+                    ? animation.iconFocus
+                    : animation.icon
+                }
                 className="icon-container"
                 style={{ width: `calc(max(${resizer.vw}vw,${resizer.px}px))` }}
                 key={index}
               >
-                <FontAwesomeIcon className="icon" icon={["fas", "male"]} />
+                <FontAwesomeIcon
+                  className={`icon ${
+                    index < content.highlightNum ? "focus" : ""
+                  }`}
+                  icon={content.icon.split(" ")}
+                />
               </motion.div>
             );
           })}
